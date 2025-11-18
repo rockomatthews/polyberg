c60c3b219b27d35e1c5737375f71bf6077800751
rockomatthews <rob@robmatthe.ws>
2025-11-18 15:00:07 -0700

    adding google auth

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,10 +1,9 @@
+import { DefaultSession } from 'next-auth';
+
 declare module 'next-auth' {
-  interface Session {
-    user?: {
+  interface Session extends DefaultSession {
+    user?: DefaultSession['user'] & {
       id?: string;
-      name?: string | null;
-      email?: string | null;
-      image?: string | null;
     };
   }
 }

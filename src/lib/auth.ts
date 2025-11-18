83fda73353bd5bd26d1cc15576c70051c78fbb26
rockomatthews <rob@robmatthe.ws>
2025-11-17 21:03:12 -0700

    added google sign in 13

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,10 @@
-import type { NextAuthOptions } from 'next-auth';
+import NextAuth from 'next-auth/next';
 import GoogleProvider from 'next-auth/providers/google';
 
 const clientId = process.env.GOOGLE_CLIENT_ID ?? '';
 const clientSecret = process.env.GOOGLE_CLIENT_SECRET ?? '';
+
+type NextAuthOptions = Parameters<typeof NextAuth>[0];
 
 export const authOptions: NextAuthOptions = {
   providers: [

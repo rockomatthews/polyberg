b0f124ad7aac49841803d673393b3cf4196aa511
rockomatthews <rob@robmatthe.ws>
2025-11-18 13:14:00 -0700

    added google sign in 16

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,8 @@
 import NextAuth from 'next-auth/next';
-import type { NextRequest } from 'next/server';
-import { NextResponse } from 'next/server';
 
 import { authOptions } from '@/lib/auth';
 
-const handler = (req: NextRequest, res: NextResponse) =>
-  NextAuth(req, res, authOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
 
